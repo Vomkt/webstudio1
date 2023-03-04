85eb01643e44953d072a69ed6df20aca00d56983
Vomkt <eugene.byba@gmail.com>
2023-03-04 23:29:55 +0300

    add respon. styles

--- js/main.js
+++ js/main.js
@@ -1,40 +1,8 @@
-let $ = jQuery;
-
-$(document).ready(function(){
-    $('.team__item-wrapper').slick({
-        infinite: true,
-        slidesToShow: 4,
-        arrows: true,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        appendArrows: '.decbase-arrows',
-        prevArrow: '.decbase-arrows__btn--prev',
-        nextArrow: '.decbase-arrows__btn--next',
-        responsive: [
-            {
-              breakpoint: 1200,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-              }
-            },
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-    });
-});
-
-// $('.team__item-wrapper').slick({
-//     infinite: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 3
-//   });
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
